a8ed472b2d1f3c7bb79541ce4ea5353cd9981584
bobb <bobb@pixelplacement.com>
2009-05-26 21:15:11 +0000

    Update to throttle on everything but the iPhone.

--- unity/com/pixelplacement/scripts/iThrottle.js
+++ unity/com/pixelplacement/scripts/iThrottle.js
@@ -2,12 +2,12 @@
 //Author: Dad
 //Usage: Component
 //Methods: None 
-var capFps : float = 35.0;
-var canCap : String;
-canCap=Application.platform.ToString();
+var capFps : float = 2;
+var platform : String;
+platform=Application.platform.ToString();
 
 function Update () {
-	if(canCap=="IPhonePlayer"){
+	if(platform!="IPhonePlayer"){
 		System.Threading.Thread.Sleep(1000/capFps);
 	}
 }
