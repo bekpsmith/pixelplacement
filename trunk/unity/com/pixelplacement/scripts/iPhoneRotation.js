1a741af2469e9c768be0e5425662c97fb7ddef6d
bobb@pixelplacement.com <bobb@pixelplacement.com@39f83e12-49ae-11de-b449-6f096a66a0e6>
2009-05-26 15:38:10 +0000

    Includes addition of 'deadzone' to allow more familiar control data.

--- trunk/unity/com/pixelplacement/scripts/iPhoneRotation.js
+++ trunk/unity/com/pixelplacement/scripts/iPhoneRotation.js
@@ -1,4 +1,4 @@
-//iPhoneRotation: Retrieves iPhone rotation axis and stores static variables in range from -1 to 1.
+//iPhoneRotation: Retrieves iPhone rotation axis and stores static variables in range from -1 to 1 including a dead zone value.
 //Author: Pixelplacement
 //Usages: Component, Static
 //Methods: getRotationX, getRotationY
@@ -16,12 +16,45 @@ function Update(){
 }
 
 //Static retrievals
-public static function getRotationX():float{
-	rotationX = (Mathf.Floor(Mathf.Clamp(iPhoneInput.acceleration.x, -1, 1)*10))/10;
-	return(rotationX);
+public static function getRotationX(deadZone:float):float{
+	if(deadZone>=1){
+		Debug.LogError("ERROR: Dead zone must be less than 1!");
+		return;
+	}else{
+		rotationX = (Mathf.Floor(Mathf.Clamp(iPhoneInput.acceleration.x, -1, 1)*10))/10;
+		rotationX = rotationX-deadZone;
+		if(rotationX>=0){
+			rotationX = (rotationX-deadZone)/(1-deadZone);
+			if(rotationX<0){
+				rotationX=0;
+			}
+		}else{
+			rotationX = (rotationX+deadZone)/(1-deadZone);
+			if(rotationX>0){
+				rotationX=0;
+			}
+		}
+		return(rotationX);
+	}
 }
 
-public static function getRotationY():float{
-	rotationY = (Mathf.Floor(Mathf.Clamp(iPhoneInput.acceleration.y, -1, 1)*10))/10;
-	return(rotationY);
+public static function getRotationY(deadZone:float):float{
+	if(deadZone>=1){
+		Debug.LogError("ERROR: Dead zone must be less than 1!");
+		return;	
+	}else{
+		rotationY = (Mathf.Floor(Mathf.Clamp(iPhoneInput.acceleration.y, -1, 1)*10))/10;
+		if(rotationY>=0){
+			rotationY = (rotationY-deadZone)/(1-deadZone);
+			if(rotationY<0){
+				rotationY=0;
+			}
+		}else{
+			rotationY = (rotationY+deadZone)/(1-deadZone);
+			if(rotationY>0){
+				rotationY=0;
+			}
+		}
+		return(rotationY);
+	}
 }
