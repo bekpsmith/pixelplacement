dc552653aacdf6a181b0ddc0a7aef431766c94a9
bobb@pixelplacement.com <bobb@pixelplacement.com@39f83e12-49ae-11de-b449-6f096a66a0e6>
2009-08-06 19:32:38 +0000

    Fixed issues with "up" causing miss fire loops.

--- trunk/unity/com/pixelplacement/scripts/iTouchGUI.js
+++ trunk/unity/com/pixelplacement/scripts/iTouchGUI.js
@@ -4,6 +4,7 @@
 static var phase : iPhoneTouch;
 static var touch : iPhoneTouch;
 static var position : Vector2;
+static var touched : boolean;
 
 //Check for touch:
 function Update () {
@@ -17,18 +18,7 @@ function Update () {
 static function onFingerTouch(rect:Rect):boolean{
 	if(phase.phase==iPhoneTouchPhase.Began && iPhoneInput.touchCount>0 && phase.position.x >0 && phase.position.y >0){
 		if (rect.Contains(phase.position)){
-        	return(true);
-		}else{
-			return(false);	
-		}
-	}else{
-		return(false);
-	}
-}
-
-static function onFingerDown(rect:Rect):boolean{
-	if(phase.phase==iPhoneTouchPhase.Began && iPhoneInput.touchCount>0 && phase.position.x >0 && phase.position.y >0){
-		if (rect.Contains(phase.position)){
+			touched=true;
         	return(true);
 		}else{
 			return(false);	
@@ -42,6 +32,7 @@ static function onFingerDown(rect:Rect):boolean{
 static function onFingerHeld(rect:Rect):boolean{
 	if (rect.Contains(phase.position)){
 		if(phase.phase==iPhoneTouchPhase.Stationary || phase.phase==iPhoneTouchPhase.Moved && iPhoneInput.touchCount>0){
+			touched=true;
 			return(true);
 		}
 	}else{
@@ -51,7 +42,8 @@ static function onFingerHeld(rect:Rect):boolean{
 
 //Check for released:
 static function onFingerUp():boolean{
-	if(phase.phase==iPhoneTouchPhase.Ended){
+	if(phase.phase==iPhoneTouchPhase.Ended && touched){
+		touched=false;
 		return(true);
 	}else{
 		return(false);	
