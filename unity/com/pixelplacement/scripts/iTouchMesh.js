1eee85ec0ead7d516c9d90ce839afb6c3445eb85
bobb <bobb@pixelplacement.com>
2009-09-16 04:14:01 +0000

    Rename of original iTouch (saving this for a later need to handle mesh touch handling)

---
+++ unity/com/pixelplacement/scripts/iTouchMesh.js
@@ -0,0 +1,64 @@
+//iTouch: Global iPhone touch manager for single touch handling on meshes with colliders.
+//If you fail to get a response, ensure the mesh has a collider attached.
+//Author: Pixelplacement
+//Usage: Component, Static
+//Methods: OnFingerDown, OnFingerHeld
+
+//Example:
+/*
+if(iTouch.OnFingerDown("GasPedal")){
+	print("Down!");
+}
+	
+if(iTouch.onFingerHeld(gameObject.name)){
+	print("Held");
+}
+*/
+
+#pragma strict
+
+//Init vars:
+static var touching : String;
+static var phase : iPhoneTouchPhase;
+var rayLength : Number = 10;
+
+//Seek single touch and set 'touching' to actively touched object:
+function Update(){
+	for (var touch : iPhoneTouch in iPhoneInput.touches) {
+		phase = iPhoneInput.touches[0].phase;
+		var ray : Ray = Camera.main.ScreenPointToRay (iPhoneInput.touches[0].position);
+		Debug.DrawRay (ray.origin, ray.direction, Color.yellow);
+		var hit : RaycastHit;
+		if (Physics.Raycast (ray, hit, rayLength)){
+			touching = hit.collider.gameObject.name;
+		}else{
+			touching=null;	
+		}
+		if(iPhoneInput.touches[0].phase==iPhoneTouchPhase.Ended){
+			touching = null;
+		}
+	}
+	if(iPhoneInput.touchCount==0){
+		touching = null;
+	}
+}
+
+//Check for single hit:
+static function OnFingerDown(target:String):boolean{
+	if(phase==iPhoneTouchPhase.Began){
+		if(touching==target){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+//Check for held/dragged over hit:
+static function OnFingerHeld(target:String):boolean{
+	if(touching == target && iPhoneInput.touchCount>0){
+		return true;
+	}else{
+		return false;	
+	}
+}
