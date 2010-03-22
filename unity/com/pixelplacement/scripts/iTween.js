23b7b44fde32f87a2721111d3925b0a91ee3a973
bobb <bobb@pixelplacement.com>
2010-03-22 05:11:24 +0000

    New version!

--- unity/com/pixelplacement/scripts/iTween.js
+++ unity/com/pixelplacement/scripts/iTween.js
@@ -1,17 +1,35 @@
-//Developed by Bob Berkebile - http://www.pixelplacement.com
-//for support check out http://www.pixelplacement.com/iTween
-
+/**
+ * iTween
+ * Movement made ease for Unity3D
+ *
+ * @author		Bob Berkebile
+ * @version		1.0
+ */
 
 /*
-verifications:
-Ensure rotational solutions are accurate (I'm 99% ceratin they are)
+Licensed under the MIT License
 
-hopeful additions:
-stopAll()
-pause()
-resume()
-pauseAll()
-resumeAll()
+Copyright (c) 2009-2010 Bob Berkebile
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+http://www.pixelplacement.com/iTween
+http://code.google.com/p/pixelplacement/wiki/iTweenLicense
 */
 
 static var registers : Array = new Array();
