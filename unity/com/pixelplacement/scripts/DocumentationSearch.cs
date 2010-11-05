083d0d8e51c1f3fd09ef312a5f20035116a09fc5
bobb <bobb@pixelplacement.com>
2010-11-05 21:33:40 +0000

    Editor tool to create a nice little doc search

---
+++ unity/com/pixelplacement/scripts/DocumentationSearch.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class DocumentationSearch : EditorWindow{
+	string searchURL = "http://unity3d.com/support/documentation/ScriptReference/30_search.html?q=";
+	string term = "";
+	int recentID = -1;
+	bool showRecent = false;
+	Vector2 recentScroll;
+	List<string> recentTerms = new List<string>();
+	
+	[MenuItem("Window/Doc Search")]
+	public static void LaunchWindow(){
+		GetWindow<DocumentationSearch>(false,"Documentation");
+	}
+	
+	void OnGUI(){
+		SearchBox();		
+		ShowRecent();	
+	}
+	
+	void SearchBox(){
+		GUILayout.BeginHorizontal();
+		term = EditorGUILayout.TextField(term);
+		if(GUILayout.Button("Search") && term !=""){
+			DoSearch(term,true);
+		}
+		GUILayout.EndHorizontal();
+	}
+		
+	void ShowRecent(){
+		if(recentTerms.Count > 0){
+			showRecent = EditorGUILayout.Foldout(showRecent,"Recent Searches");
+			if(showRecent){
+				recentScroll = EditorGUILayout.BeginScrollView(recentScroll);
+				recentID = GUILayout.SelectionGrid(recentID,recentTerms.ToArray(),1);
+				EditorGUILayout.EndScrollView();
+				if(recentID != -1){
+					DoSearch(recentTerms.ToArray()[recentID],false);
+					recentID=-1;
+				}
+				ClearRecent();
+			}
+		}
+	}
+	
+	void DoSearch(string term, bool addToRecentList){
+		Application.OpenURL (searchURL + term);
+		if(addToRecentList && recentTerms.IndexOf(term) == -1){
+			recentTerms.Reverse();
+			recentTerms.Add(term);
+			recentTerms.Reverse();	
+		}
+	}
+
+	void ClearRecent(){
+		EditorGUILayout.Separator();
+		if(GUILayout.Button("Clear Recent Searches")){
+			recentTerms=new List<string>();
+		}	
+	}
+}
+
