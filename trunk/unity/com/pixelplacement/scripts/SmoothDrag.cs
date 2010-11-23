c6b9334b10da4f9e4a1ebfc9fba43fead577c5cf
bobb@pixelplacement.com <bobb@pixelplacement.com@39f83e12-49ae-11de-b449-6f096a66a0e6>
2010-11-23 04:38:28 +0000

    A very concise script for each finger dragging of objects with nice easing

---
+++ trunk/unity/com/pixelplacement/scripts/SmoothDrag.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class SmoothDrag : MonoBehaviour {
+	float distance = 0;
+	Vector3 targetPosition = Vector3.zero;
+	Transform dragObject;
+	
+	void Update () {		
+		if(Input.touchCount>0){
+			
+			//set the object to be dragged or clear out a previouslt dragged object:
+			if(Input.GetTouch(0).phase == TouchPhase.Began){
+				Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit, 100) && hit.collider.tag == "Moveable"){
+					distance = hit.distance;
+					dragObject = hit.collider.transform;
+					targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, distance));
+				}else{
+					dragObject = null;
+				}
+			}
+			
+			//recalcualte the target position if we detect a finger movement:
+			if(Input.GetTouch(0).phase == TouchPhase.Moved){
+				targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, distance));
+			}			
+		}
+		
+		//update the target object's position at a speed of 6 with iTween's Vector3Update calculation for real-time easing:
+		if(dragObject){
+			dragObject.position = iTween.Vector3Update(dragObject.position, new Vector3(targetPosition.x,targetPosition.y,dragObject.position.z), 6);
+		}
+	}
+
+}
