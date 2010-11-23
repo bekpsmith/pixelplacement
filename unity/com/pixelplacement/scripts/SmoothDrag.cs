18fbff1ad87286c27016ace80b8b0f614820269c
bobb <bobb@pixelplacement.com>
2010-11-23 04:46:03 +0000

    Added offset

--- unity/com/pixelplacement/scripts/SmoothDrag.cs
+++ unity/com/pixelplacement/scripts/SmoothDrag.cs
@@ -5,6 +5,7 @@ public class SmoothDrag : MonoBehaviour {
 	float distance = 0;
 	Vector3 targetPosition = Vector3.zero;
 	Transform dragObject;
+	Vector3 offset;
 	
 	void Update () {		
 		if(Input.touchCount>0){
@@ -16,6 +17,8 @@ public class SmoothDrag : MonoBehaviour {
 				if(Physics.Raycast(ray, out hit, 100) && hit.collider.tag == "Moveable"){
 					distance = hit.distance;
 					dragObject = hit.collider.transform;
+					offset = dragObject.position -  new Vector3(hit.point.x, hit.point.y, dragObject.position.z);
+					Debug.Log(offset);
 					targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, distance));
 				}else{
 					dragObject = null;
@@ -30,7 +33,7 @@ public class SmoothDrag : MonoBehaviour {
 		
 		//update the target object's position at a speed of 6 with iTween's Vector3Update calculation for real-time easing:
 		if(dragObject){
-			dragObject.position = iTween.Vector3Update(dragObject.position, new Vector3(targetPosition.x,targetPosition.y,dragObject.position.z), 6);
+			dragObject.position = iTween.Vector3Update(dragObject.position, new Vector3(targetPosition.x + offset.x,targetPosition.y + offset.y,dragObject.position.z), 6);
 		}
 	}
 
