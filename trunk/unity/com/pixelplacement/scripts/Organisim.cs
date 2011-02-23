e05c7840ba971712b3306c500e00a2168760c1a3
bobb@pixelplacement.com <bobb@pixelplacement.com@39f83e12-49ae-11de-b449-6f096a66a0e6>
2011-02-23 03:46:43 +0000

    Random movement and rotation

---
+++ trunk/unity/com/pixelplacement/scripts/Organisim.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class Organisim : MonoBehaviour
+{
+	float dampen = .98f;
+	float vx, vy, vz, vrx, vry, vrz;
+	float randomRange = .0005f;
+	Transform _transform;
+	
+	void Start(){
+	 _transform = transform;	
+	}
+
+	void Update(){
+		vx+=Random.Range(-randomRange,randomRange);
+		vy+=Random.Range(-randomRange,randomRange);
+		vz+=Random.Range(-randomRange,randomRange);
+		vrx+=Random.Range(-randomRange,randomRange);
+		vry+=Random.Range(-randomRange,randomRange);
+		vrz+=Random.Range(-randomRange,randomRange);	
+	
+		//random position:
+		Vector3 newPos = _transform.position;
+		newPos.x+=vrx;
+		newPos.y+=vry;
+		newPos.z+=vrz;
+		_transform.position = newPos;
+		
+		//random rotation:
+		Quaternion newRot = _transform.rotation;
+		newRot.x+=vrx;
+		newRot.x+=vry;
+		newRot.x+=vrz;
+		_transform.rotation = newRot;
+
+		vx *= dampen;
+		vy *= dampen;
+		vz *= dampen;
+		vrx *= dampen;
+		vry *= dampen;
+		vrz *= dampen;
+	}
+}
