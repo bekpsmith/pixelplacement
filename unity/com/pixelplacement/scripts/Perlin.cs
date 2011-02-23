d1229690ec6d092d7f17d408f29fce08221e57f0
bobb <bobb@pixelplacement.com>
2011-02-23 04:48:04 +0000

    Added 3 organic movement solutions Added Unity's Perlin implementation into previous file

--- unity/com/pixelplacement/scripts/Perlin.cs
+++ unity/com/pixelplacement/scripts/Perlin.cs
@@ -2,6 +2,44 @@ using System.Collections;
 using System;
 using UnityEngine;
 
+/* Perlin noise use example:
+
+Perlin perlin = new Perlin();
+var value : float = perlin.Noise(2);
+var value : float = perlin.Noise(2, 3, );
+var value : float = perlin.Noise(2, 3, 4);
+
+
+SmoothRandom use example:
+
+var p = SmoothRandom.GetVector3(3);
+
+*/
+
+public class SmoothRandom
+{
+	public static Vector3 GetVector3 (float speed)
+	{
+		float time = Time.time * 0.01F * speed;
+		return new Vector3(Get().HybridMultifractal(time, 15.73F, 0.58F), Get().HybridMultifractal(time, 63.94F, 0.58F), Get().HybridMultifractal(time, 0.2F, 0.58F));
+	}
+	
+	public static float Get (float speed)
+	{
+		float time = Time.time * 0.01F * speed;
+		return Get().HybridMultifractal(time * 0.01F, 15.7F, 0.65F);
+	}
+
+	private static FractalNoise Get () { 
+		if (s_Noise == null)
+			s_Noise = new FractalNoise (1.27F, 2.04F, 8.36F);
+		return s_Noise;		
+	 }
+
+	private static FractalNoise s_Noise;
+}
+
+
 public class Perlin
 {
 	// Original C code derived from 
