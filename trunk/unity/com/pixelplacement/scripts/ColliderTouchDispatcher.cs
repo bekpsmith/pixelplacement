a906ba2422cab6a579fbb28bbba583a9e6743a92
pixelplacement <pixelplacement@39f83e12-49ae-11de-b449-6f096a66a0e6>
2012-05-05 15:26:05 +0000

    Consolidated touch detection to one class and killed some super horrible old stuff

--- trunk/unity/com/pixelplacement/scripts/ColliderTouchDispatcher.cs
+++ trunk/unity/com/pixelplacement/scripts/ColliderTouchDispatcher.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class ColliderTouchDispatcher : MonoBehaviour {
 	
+	public static event System.Action<RaycastHit> OnTouch;
 	public Camera renderingCamera;
 	public bool useTouch = true;
 	public bool useMouse = true;
-	static ColliderTouchDispatcher _instance;
+	static ColliderTouchDispatcher _instance = null;
 	
 	void Awake(){
 		//duplicate instance check:
@@ -29,18 +30,22 @@ public class ColliderTouchDispatcher : MonoBehaviour {
 					Ray ray = renderingCamera.ScreenPointToRay( new Vector3( touch.position.x, touch.position.y, 0 ) );
 					RaycastHit rayCastHit;
 					if ( Physics.Raycast( ray, out rayCastHit, renderingCamera.farClipPlane ) ) {
-						Debug.Log( rayCastHit.collider.name );	
+						if ( OnTouch != null ) {
+							OnTouch( rayCastHit );
+						}	
 					}
 				}
 			}
 		}
 		
 		if ( useMouse ) {	
-			if ( Input.GetMouseButton( 0 ) ) {
+			if ( Input.GetMouseButtonDown( 0 ) ) {
 				Ray ray = renderingCamera.ScreenPointToRay( new Vector3( Input.mousePosition.x, Input.mousePosition.y, 0 ) );
 				RaycastHit rayCastHit;
 				if ( Physics.Raycast( ray, out rayCastHit, renderingCamera.farClipPlane ) ) {
-					Debug.Log( rayCastHit.collider.name );	
+					if ( OnTouch != null ) {
+						OnTouch( rayCastHit );
+					}	
 				}
 			}
 		}
